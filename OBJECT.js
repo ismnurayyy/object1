2765fbd12cbfbeee90e13cbb1f2d7b3414ce0b32
ismnurayy <ismnnuray1@gmail.com>
2024-04-01 02:19:18 +0400

    object

---
+++ OBJECT.js
@@ -0,0 +1,81 @@
+// 1. Obyekt yaradin.
+// 2. Array icinde coxlu obyektler yaradin
+// 3. Telebeler obyektinde bir nece telebe elave edin. Hobbileri, xususiyettleri kimi icerisindede deyerleri qeyd edin.
+// 4. Students arrayinin qiymet ortalamasini cixardin. (Butun telebelerin qiymetlerini alib sayini bolun.)
+// 5. Qiymeti 90-dan az olan telebeleri Ad ve Soyadi ile birlikde ekrana cixardib onlara xeberdarliq edin.
+// let telebeler = [
+//     {
+//         id:1,
+//         Ad: "Nuray",
+//         Soyad: "Ismayilova",
+//         Ortalama: 88,
+//         Xususiyyetler:["sakit","calisqan"],
+//         Hobbi: ["kitab oxumaq", "arasdirma etmek"],
+//         group_id: 11
+//     },
+//     {
+//         id:1,
+//         Ad: "Rena",
+//         Soyad: "Ceferzade",
+//         Ortalama: 94,
+//         Xususiyyetler:["sakit","agilli"],
+//         Hobbi: ["kitab oxumaq", "anime izlemek"],
+//         group_id: 11
+//     },
+//     {
+//         id:1,
+//         Ad: "Gunay",
+//         Soyad: "Babayeva",
+//         Ortalama: 95,
+//         Xususiyyetler:["sozunu deyen","calisqan"],
+//         Hobbi: ["kitab oxumaq", "resm cekmek"],
+//         group_id: 22
+//     },
+//     {
+//         id:1,
+//         Ad: "Nahid",
+//         Soyad: "Allahverdiyev",
+//         Ortalama: 80,
+//         Xususiyyetler:["sozunu deyen","agilli"],
+//         Hobbi: ["futbol oynamaq", "gitar calmaq"],
+//         group_id: 22
+//     },
+//     {
+//         id:1,
+//         Ad: "Meleyke",
+//         Soyad: "Huseynova",
+//         Ortalama: 95,
+//         Xususiyyetler:["sozunu deyen","calisqan"],
+//         Hobbi: [" bir seyleri davamli etmez"],
+//         group_id: 11
+//     }
+// ]
+// let groups = [
+//     {
+//         id:11,
+//         Ad:"Komputer Muhendisliyi ||"
+//     },
+//     {
+//         id:22,
+//         Ad:"Komputer Muhendisliyi |"
+//     }
+// ]
+    // let cem = 0;
+    // for(let i = 0; i < telebeler.length; i++) {
+    //     cem += telebeler[i].Ortalama;
+        
+        
+    // }
+    // let edediorta = cem / telebeler.length;
+    //     console.log(` "Students arrayinin qiymet ortalamasi :"${edediorta}`);
+
+
+
+        // console.log(`${telebeler[i].Ad} ${telebeler[i].Soyad} ${telebeler[i].Ortalama}`);
+    
+// telebeler.forEach(function(qiymet) {
+    //     if(qiymet.Ortalama<90){
+    //         console.log( `${ qiymet.Ad } ${ qiymet.Soyad} ${ qiymet.Ortalama}` );
+    //         console.log(" Ortalamanız Aşağıdır")
+    //     }
+    // });
